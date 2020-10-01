e434bb0efc15815878bf97d4186e03bcedd4dc87
dayannapatricia <pdayanamartinez@gmail.com>
2020-10-01 16:58:28 -0500

    se crea el proyecto en react con algunas actualizaciones

---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const register= ( ) =>{
+    return (
+   <h1>registro en psico felix</h1>
+   
+
+    )
+};export default register;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+
+
+
+
+function Home () {
+  return (
+    <>
+    <p>Esta es mi página de home </p>
+    </>
+  );
+}
+
+export default Home;
