77976202455eeb7d7d96edaf9a12b4c3a099c1b3
Karen Alexandra Gordon Duque <lexandra.gordon29@gmail.com>
2020-11-19 17:19:19 +0000

    Actualización de la pagina respuesta, y se agrego la ruta del chat del psicólogo

--- frontend/src/pages/Respuesta.js
+++ frontend/src/pages/Respuesta.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 
-import '../Estilos/Acercade.css';
+import '../Estilos/respuesta.css';
 
-class Respuesta extends Component {
+class respuesta extends Component {
     
     render() { 
         return (
     <>
-   <h1>Acerca de</h1> 
+   <h1>Chatear con el Psicologo</h1> 
 
- <h2 className='chat' > Chat</h2>
- <p className='chat'> Encontrará todos los chats de los estudiantes que solicitan una asesoría</p>
- <h2 className='acerca'>Acerca de </h2>
- <p className='acerca'>Este  botón sirve para ver cuál es la funcion de el resto  de los botones </p>
- <h2 className='quien'>Quienes somos</h2>
- <p className='quien'>Este te ayudara a visualizar a los integrantes del equipo</p>
- <h2 className='info'>Más información</h2>
- <p className='info'>Se encontrará mas información sobre el proyecto</p>
- <h2 className='sigui'>Siguiente</h2>
- <p className='sigui'>Este boton te llevara al registro y/o inicio</p></>
+ 
+ <p className='chat'> Chatea con nuestro servicio de Psico-Félix aquí</p>
+
+ <h2>Chat del Psicolo-Félix</h2>
+ <p>https://app.jivosite.com/chat/my/11</p>
+ 
+ </>
       );
       
     }}
-    export default Respuesta;
+    export default respuesta;
