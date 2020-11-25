c07cc455bde18c8d9fc47f5141434821666a5a1b
Dayanna Patricia Martínez Morelo <pdayanamartinez@gmail.com>
2020-11-25 01:34:11 +0000

    Santiago y dayanna organizan y actualizan base de datos

--- Backend_appi_rest/routes/usuarios.js
+++ Backend_appi_rest/routes/usuarios.js
@@ -2,80 +2,76 @@ const express = require('express');
 const router =express.Router();
 const mysqlConnection = require('../db/db');
 
+
 router.get('/',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM usuarios',(err,rows,fiels)=>{
-        if(!err){
-            //haga esto
-            res.json(rows);
-        }else{
-            console.log(err);
-        }//fin si
-    })//fin query
-    
-    
-    })//retorno de todos los estudiantes
+   mysqlConnection.query('SELECT * FROM usuarios',(err,rows,fiels)=>{
+       if(!err){
+           //haga esto
+           res.json(rows);
+       }else{
+           console.log(err);
+}});//fin si
+})// fin  query
+//guardar un nuevo usuario
+router.post('/nuevo',(req,res)=>{
+const{documento_id,genero,cargo,fecha_de_nacimiento,contraseña}=req.body;
 
-<<<<<<< HEAD
-    router.post('/nuevo-usuario',(req,res)=>{
-=======
-    router.post('/nuevo-registro',(req,res)=>{
->>>>>>> 6d75d1c3f67f4b48e87575655657264a6fe89e7d
-        const {documento_id,genero,cargo,fecha_de_nacimiento}=req.body;
-        
-        let alumno =[documento_id,genero,cargo,fecha_de_nacimiento];
-        
-        let nuevoAlumno =`INSERT INTO usuarios(documento_id,genero,cargo,fecha_de_nacimiento)
-        VALUES(?,?,?,?)`;
-        mysqlConnection.query(nuevoAlumno,alumno,(err,results,fields)=>{
-        if(err){
-           return console.error(err.message());
-        }else{
-           res.json({message:`Registro correcto`})
-        }
-        })});
-<<<<<<< HEAD
-        //actualizar un usuario por su documento id 
-        router.post('/usuario/:documento_id',(req,res)=>{
-=======
-        router.post('/estudiante/:id',(req,res)=>{
->>>>>>> 6d75d1c3f67f4b48e87575655657264a6fe89e7d
-           const {documento_id,genero,cargo,fecha_de_nacimiento}=req.body;
-           const {documento_id} =req.body;
-           
-           
-<<<<<<< HEAD
-           mysqlConnection.query(`UPDATE usuarios SET cargo=?,genero=?,fecha_de_nacimiento=?  WHERE documento_id=?`,
-      [cargo,genero,fecha_de_nacimiento,documento_id,],(err,rows,fields)=>{
-=======
-           mysqlConnection.query(`UPDATE usuarios SET documento_id=?,cargo=?,genero=?,fecha_de_nacimiento=?,`,
-      [documento_id,fecha_de_nacimiento,cargo,genero],(err,rows,fields)=>{
->>>>>>> 6d75d1c3f67f4b48e87575655657264a6fe89e7d
-           if(!err){
-              
-              res.json({status:`Datos actualizados`});
-           }else{
-              console.log(err);
-             
-           }
-           })});
-        
-<<<<<<< HEAD
-           router.get('/usuarios/:documento_id',(req,res)=>{
-=======
-           router.get('/estudiante/:documento_id',(req,res)=>{
->>>>>>> 6d75d1c3f67f4b48e87575655657264a6fe89e7d
-              const {id} =req.params;
-              mysqlConnection.query('SELECT * from usuarios where documento_id= ? ',[documento_id],(err,rows,fields)=>{
-              if(!err){
-                 res.json(rows); 
-              }else{
-              console.log(err);
-              }});  })// fin
+let alumno =[documento_id,genero,cargo,fecha_de_nacimiento,contraseña];
 
-<<<<<<< HEAD
-              //inicio sesion
+let nuevoAlumno =`INSERT INTO usuarios(documento_id,genero,cargo,fecha_de_nacimiento,contraseña)
+VALUES(?,?,?,?,?)`;
+mysqlConnection.query(nuevoAlumno,alumno,(err,results,fields)=>{
+if(err){
+
+   return console.error(err.message);
+}else{
+   res.json({message:`Registro correcto`})
+}
+})});
+//actualizar los datos de un usuario
+router.put('/actualizar/:id',(req,res)=>{
+   const {genero,cargo,fecha_de_nacimiento,contraseña}=req.body;
+   const {id} =req.params;
+   
+
+   mysqlConnection.query(`UPDATE usuarios SET cargo=?,genero=?,fecha_de_nacimiento=?,contraseña=? WHERE documento_id=?`,
+   [cargo,genero,fecha_de_nacimiento,contraseña, id],(err,rows,fields)=>{
+   if(!err ){//si no hay error
+      res.json({message:`Usuario actualizado`})
+    
+   }else{
+      console.log(err);
+     
+   }
+   })});
+
+ //consultar un estudiante por su documento id
+      router.get('/consultar/:id',(req,res)=>{
+      const {id} =req.params;
+      mysqlConnection.query('SELECT * from usuarios where documento_id= ? ',[id],(err,rows,fields)=>{   
+      if(!err){
+         res.json(rows); 
+      }else{
+      console.log(err);
+      }});
+      })// fin
+
+      //eliminar un id
+      router.delete('/eliminar/:id',(req,res)=>{
+         const {id} =req.params;
+         mysqlConnection.query('DELETE  from usuarios where documento_id= ? ',[id],(err,rows,fields)=>{   
+         if(!err){
+            res.json({message:`Usuario eliminado`})
+
+         }else{
+         console.log(err);
+         }});
+         })// fin
+   
+
+          //inicio sesion
                  
-           router.get('/login',(req,res)=>{
+          router.get('/login',(req,res)=>{
             const {documento_id, contraseña} =req.params;
             mysqlConnection.query('SELECT * from usuarios WHERE documento_id= ?  AND contraseña=?',[documento_id, contraseña],(err,rows,fields)=>{
             if(!err){
@@ -83,9 +79,5 @@ router.get('/',(req,res)=>{
             }else{
             console.log(err);
             }});  })// fin
-           
 
-module.exports =router;
-=======
-module.exports =router;
->>>>>>> 6d75d1c3f67f4b48e87575655657264a6fe89e7d
+module.exports=router;
